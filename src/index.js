6e78d59c7c34861078a16191c4aa2602ca3ef930
Pulkit Sharma <pulkit@simformsolutions.com>
2023-08-16 17:49:34 +0530

    login and register page updated

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { env_obj } from './env';
+
+// for redux
+import store from './store/store';
+import { Provider } from 'react-redux';
+
+const client = new ApolloClient({
+  uri: env_obj['BACKEND_URL'],
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
